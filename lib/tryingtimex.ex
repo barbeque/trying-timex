1acb17c002f3a4e7241c7fd386e791269ef2c454
mike stedman <github@ravuya.com>
2017-02-08 11:51:53 -0700

    add more shit

--- lib/tryingtimex.ex
+++ lib/tryingtimex.ex
@@ -16,13 +16,16 @@ defmodule Tryingtimex do
     :world
   end
 
-  def get_todays_week_range do
-    today = Timex.now
-      |> Timex.iso_triplet
+  def get_week_range(date) do
+    triplet = date |> Timex.iso_triplet
     # starts on monday i guess
-    start_week = today |> fn({year, week, _}) -> {year, week, 1} end.() |> Timex.from_iso_triplet
-    end_week = today |> fn({year, week, _}) -> {year, week, 7} end.() |> Timex.from_iso_triplet
+    start_week = triplet |> fn({year, week, _}) -> {year, week, 1} end.() |> Timex.from_iso_triplet
+    end_week = triplet |> fn({year, week, _}) -> {year, week, 7} end.() |> Timex.from_iso_triplet
     # return it so everyone is happy
     { start_week, end_week }
+  end
+
+  def get_todays_week_range do
+    get_week_range Timex.now
   end
 end
